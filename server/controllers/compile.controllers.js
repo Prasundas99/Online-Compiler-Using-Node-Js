540c8d8d8e65439a276fbefe24ac379aba87c447
Prasundas99 <prasundas115@gmail.com>
2021-08-22 15:25:20 +0530

    [NEW] python execution

--- server/controllers/compile.controllers.js
+++ server/controllers/compile.controllers.js
@@ -1,22 +1,30 @@
 import asyncHandler from "express-async-handler";
 import { generateFile } from "../utils/generateFile.js";
+import {executePy} from '../utils/executeCode.js'
 
 /**
  * @description Compile/run the inputted code
- * @route GET /run
+ * @route POST /run
  * @public
  *
  */
 export const compileCode = asyncHandler(async (req, res) => {
-  const { language = "cpp", code } = req.body;
+  const { language = "py", code } = req.body;
   console.log(req.body);
   //Check if any code is passed or not
   if (code === undefined) {
     return res.status(400).json({ success: false, error: "Empty code body!" });
   }
-
-  //generate C++ file with content from request
+try {
+  //generate a code file with content from request
   const filePath = await generateFile(language, code);
 
-  res.status(200).json({ filePath, language, code });
+  //We need to run the file and send the output
+  const output = await executePy(filePath)
+  console.log(output);
+  res.status(200).json({ filePath, language, code, output });
+} catch (error) {
+  res.status(500).json({error})
+}
+  
 });
