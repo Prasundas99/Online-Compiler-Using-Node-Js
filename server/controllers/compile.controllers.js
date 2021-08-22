1f590c02c0609b4e7e1f52fafc49a2fce88b29c4
Prasundas99 <prasundas115@gmail.com>
2021-08-23 00:38:54 +0530

    [FIX] python compile bug fix

--- server/controllers/compile.controllers.js
+++ server/controllers/compile.controllers.js
@@ -1,6 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { generateFile } from "../utils/generateFile.js";
-import {executePy} from '../utils/executeCode.js'
+import { executePy } from "../utils/executeCode.js";
 
 /**
  * @description Compile/run the inputted code
@@ -11,20 +11,19 @@ import {executePy} from '../utils/executeCode.js'
 export const compileCode = asyncHandler(async (req, res) => {
   const { language = "py", code } = req.body;
   console.log(req.body);
+
   //Check if any code is passed or not
   if (code === undefined) {
     return res.status(400).json({ success: false, error: "Empty code body!" });
   }
-try {
-  //generate a code file with content from request
-  const filePath = await generateFile(language, code);
-
-  //We need to run the file and send the output
-  const output = await executePy(filePath)
-  console.log(output);
-  res.status(200).json({ filePath, language, code, output });
-} catch (error) {
-  res.status(500).json({error})
-}
-  
+  try {
+    //generate a code file with content from request
+    const filePath = await generateFile(language, code);
+    //We need to run the file and send the output
+    const output = await executePy(filePath);
+    console.log(output);
+    res.status(200).json({ filePath, language, code, output });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
--- server/utils/executeCode.js
+++ server/utils/executeCode.js
@@ -9,8 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 //sellects a folder name "code" which is one level abouve this directry
-const outputPath = path.join(__dirname, "..", "output");
-console.log("output Path:", outputPath);
+const outputPath = path.join(__dirname, "../", "output");
 //creates the folder "output" if it dosnt exist
 if (!fs.existsSync(outputPath)) {
     fs.mkdirSync(outputPath, { recursive: true });
@@ -21,10 +20,9 @@ if (!fs.existsSync(outputPath)) {
 export const executePy = (filepath) => {
     const jobId = path.basename(filepath).split(".")[0];
     const outPath = path.join(outputPath, `${jobId}.out`);
-  console.log(jobId, "JODID", outPath);
     return new Promise((resolve, reject) => {
         exec(
-          `python ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
+          `python ${filepath} -o ${outPath} && cd ${outputPath}`,
           (error, stdout, stderr) => {
             error && reject({ error, stderr });
             stderr && reject(stderr);
